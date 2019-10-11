d9fd592676c4253d53e9257e50c589589614d9ce
sunmonu oluwole <sunmonuoluwole@gmail.com>
2019-10-11 14:37:08 +0100

    deleted src

--- src/App.js
+++ src/App.js
@@ -3,9 +3,8 @@ import Header from './components/header/header';
 import About from './components/about/about';
 import Resume from './components/resume/resume';
 import Portfolio from './components/portfolio/portfolio';
-import Contactus from './components/contactus/contactus';
 import Testimonials from './components/testimonials/testimonial';
-import Footer from './components/footer/footer'
+
 
 class App extends Component {
   render(){
